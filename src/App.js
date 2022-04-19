02dad0a0b3c01fd652437c85f494d02ab01ff835
EdoardoGenovese <genovese.edo4rdo@gmail.com>
2022-04-20 00:16:54 +0200

    footer e page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import 'bootstrap/dist/js/bootstrap'
+
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navabar/Navbar';
+import Posts from './components/posts/Posts'
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Posts />}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Posts () {
+    fetch('http://wordpress.test/wp-json/wp/v2/posts')
+        .then(response => response.json())
+        .then(posts => {
+
+            const body = document.getElementById('body-post');
+
+            console.log(posts)
+            
+            body.innerHTML = ''
+
+            for(const post of posts){
+
+                body.innerHTML += `
+                    <h4>${post.title.rendered}</h4>
+                    <p>${post.content.rendered}</p>
+                `
+
+            }
+        })
+    
+    return(
+        <div id="body-post">
+
+        </div>
+    )
+}
